347d12726386f59a28276cae720a6dde60c0a7c4
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 11:14:02 +0600

    Printed character variable

--- first.c
+++ first.c
@@ -7,7 +7,10 @@ int main()
 
     float num2 = 4.56;
 
+    char character = '8';
+
     printf("The integer number is %d\n", num1);
-    printf("The float number is %.2f", num2);
+    printf("The float number is %.2f\n", num2);
+    printf("The character is %c", character);
     return 0;
 }
