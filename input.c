dbf9d69260ef8045a8a654d47cd5afa59248f66b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 19:19:41 +0600

    Took integer, float and character as input
    Finally, printed the input values

--- input.c
+++ input.c
@@ -3,11 +3,15 @@
 int main()
 {
     int num1;
+    float num2;
+    char character1;
 
-    printf("Enter a number : ");
-    scanf("%d", &num1);
+    printf("Enter a integer number, a float number, a character : ");
+    scanf("%d %f %c", &num1, &num2, &character1);
 
-    printf("The number is : %d", num1);
+    printf("The integer number is : %d\n", num1);
+    printf("The float number is : %f\n", num2);
+    printf("The character is : %c", character1);
 
     return 0;
 }
