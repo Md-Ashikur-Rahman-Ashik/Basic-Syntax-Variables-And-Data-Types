bedabcd1951ef51ae741c9e532ae910a7044808e
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-06 10:59:08 +0600

    Printed float variable

--- first.c
+++ first.c
@@ -5,6 +5,9 @@ int main()
     int num1 = 10;
     num1 = 20;
 
-    printf("%d", num1);
+    float num2 = 4.56;
+
+    printf("The integer number is %d\n", num1);
+    printf("The float number is %.2f", num2);
     return 0;
 }
